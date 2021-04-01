fac7d09c0611ebb3a7779ffa16217a7f5ab0effb
du-java <du_java@bk.ru>
2021-04-01 18:54:25 +0200

    lesson15

---
+++ src/lesson15/App01.java
@@ -0,0 +1,12 @@
+package lesson15;
+
+public class App01 {
+    public static void main(String[] args) {
+
+        TodoRepository todoRepository = new TodoRepository();
+
+        todoRepository.getTodos("asd");
+
+
+    }
+}
