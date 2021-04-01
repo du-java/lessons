5dcf59c2e3d861adfa451e74721c1e3de87b975f
olga.prokopova <lookluckalisa@gmail.com>
2021-04-01 20:15:46 +0300

    added

--- src/lesson15/TodoRepository.java
+++ src/lesson15/TodoRepository.java
@@ -1,20 +1,32 @@
 package lesson15;
 
 import java.util.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class TodoRepository {
 
     private Map<ToDoType, List<Todo>> todos = new HashMap<>();
+    private static final Logger logger = Logger.getLogger(TodoRepository.class.getName());
+
 
     public List<Todo> getTodos(String type) {
-        List<Todo> todos = this.todos.get(ToDoType.valueOf(type.toUpperCase(Locale.ROOT)));
-        if(todos == null){
+        try {
+            List<Todo> todos = this.todos.get(ToDoType.valueOf(type.toUpperCase(Locale.ROOT)));
+            if (todos == null) {
+                return Collections.emptyList();
+            }
+            return todos;
+        } catch (IllegalArgumentException e) {
+            logger.log(
+                    Level.SEVERE, e.getMessage(), e
+            );
             return Collections.emptyList();
         }
-        return  todos;
     }
 
-    public void add(String type,Todo todo) {
+
+    public void add(String type, Todo todo) {
 
     }
 }
