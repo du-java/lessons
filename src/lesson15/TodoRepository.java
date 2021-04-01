b3159ddce1b40b609bac206b75f312df896f07a5
AlexOak <dubovec995@gmail.com>
2021-04-01 20:30:36 +0300

    Lesson15

--- src/lesson15/TodoRepository.java
+++ src/lesson15/TodoRepository.java
@@ -6,13 +6,13 @@ import java.util.logging.Logger;
 
 public class TodoRepository {
 
-    private Map<ToDoType, List<Todo>> todos = new HashMap<>();
+    private Map<ToDoType, List<Todo>> map = new HashMap<>();
     private static final Logger logger = Logger.getLogger(TodoRepository.class.getName());
 
 
     public List<Todo> getTodos(String type) {
         try {
-            List<Todo> todos = this.todos.get(ToDoType.valueOf(type.toUpperCase(Locale.ROOT)));
+            List<Todo> todos = this.map.get(ToDoType.valueOf(type.toUpperCase(Locale.ROOT)));
             if (todos == null) {
                 return Collections.emptyList();
             }
@@ -27,6 +27,14 @@ public class TodoRepository {
 
 
     public void add(String type, Todo todo) {
+        ToDoType key = ToDoType.valueOf(type.toUpperCase(Locale.ROOT));
+        List<Todo> todos = this.map.get(key);
 
+        if (todos == null) {
+            map.put(key, Arrays.asList(todo));
+        } else {
+            todos.add(todo);
+            map.put(key,todos);
+        }
     }
 }
