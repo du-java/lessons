4909873ffa1c7bc028267c83c7377650b8b6052f
Dimacom <lqs5283829>
2021-04-06 21:57:03 +0300

    Merge branch 'main' of C:\Users\User\IdeaProjects\lessons with conflicts.

--- src/lesson15/TodoRepository.java
+++ src/lesson15/TodoRepository.java
@@ -9,7 +9,6 @@ import java.util.logging.Logger;
 public class TodoRepository {
 
     private Map<ToDoType, List<Todo>> map = new HashMap<>();
-
     private static final Logger logger = Logger.getLogger(TodoRepository.class.getName());
 
     public List<Todo> getTodos(ToDoType type) {
@@ -26,8 +25,6 @@ public class TodoRepository {
         } catch (IllegalArgumentException e) {
             logger.log(Level.SEVERE, e.getMessage(), e);
             return Collections.emptyList();
-//            throw  new IllegalArgumentException();
-//            throw e;
         }
     }
 
@@ -38,6 +35,7 @@ public class TodoRepository {
     public void add(String type, Todo todo) {
         ToDoType key = ToDoType.valueOf(type.toUpperCase(Locale.ROOT));
         List<Todo> todos = this.map.get(key);
+
         if (todos == null) {
             map.put(key, Collections.singletonList(todo));
         } else {
@@ -47,7 +45,7 @@ public class TodoRepository {
         }
     }
 
-    public List<Task> getTasksByDate(LocalDate date) {
+    public List<Task> getTasks(LocalDate date) {
         List<Task> taskList = new ArrayList<>();
         for (Todo todo : getTodos(ToDoType.TASK)) {
             Task task = (Task) todo;
@@ -58,8 +56,9 @@ public class TodoRepository {
         return taskList;
     }
 
-    public List<Event> getEventsByTime(LocalDateTime time) {
-        List<Event> taskList = new ArrayList<>();
+    public List<Event> getEvents(LocalDateTime time) {
+
+        List<Event     > taskList = new ArrayList<>();
         for (Todo todo : getTodos(ToDoType.EVENT)) {
             Event event = (Event) todo;
             if (time.isAfter(event.getStartTime()) && time.isBefore(event.getEndTime())) {
@@ -67,92 +66,6 @@ public class TodoRepository {
             }
         }
         return taskList;
-    }
-
-    public List<Task> getSortedTasksByTime() {
-        List<Task> taskList = getTaskList();
-        taskList.sort(new Comparator<Task>() {
-            @Override
-            public int compare(Task o1, Task o2) {
-                return o1.getTime().compareTo(o2.getTime());
-            }
-        });
-        return taskList;
-    }
-
-    public List<Task> getSortedTasksByCount() {
-        List<Task> taskList = getTaskList();
-        Comparator<Task> comparator = new Comparator<Task>() {
-            @Override
-            public int compare(Task o1, Task o2) {
-                return o1.getSubTaskList().size() - o2.getSubTaskList().size();
-            }
-        };
-        taskList.sort(comparator.reversed());
-        return taskList;
-    }
-
-    public List<Event> getSortedEventsByStartTime() {
-        List<Event> eventList = getEventList();
-        eventList.sort(new Comparator<Event>() {
-            @Override
-            public int compare(Event o1, Event o2) {
-                return o1.getStartTime().compareTo(o2.getStartTime());
-            }
-        });
-        return eventList;
-    }
-
-    public List<Event> getSortedEventsByBusy() {
-        List<Event> eventList = getEventList();
-        eventList.sort(new Comparator<Event>() {
-            @Override
-            public int compare(Event o1, Event o2) {
-                return Boolean.compare(o2.isBusy(), o1.isBusy());
-            }
-        });
-        return eventList;
-    }
-
-    private List<Event> getEventList() {
-        List<Event> events = new ArrayList<>();
-        for (Todo todo : getTodos(ToDoType.EVENT)) {
-            Event event = (Event) todo;
-            events.add(event);
-        }
-        return events;
-    }
-
-    private List<Task> getTaskList() {
-        List<Task> taskList = new ArrayList<>();
-        for (Todo todo : getTodos(ToDoType.TASK)) {
-            Task task = (Task) todo;
-            taskList.add(task);
-        }
-        return taskList;
-    }
-
-    private <K, V> Map<V, K> swapKVToVK(Map<K, V> map) {
-        Map<V, K> swapMap = new HashMap<>();
-        for (Map.Entry<K, V> entry : map.entrySet()) {
-            swapMap.put(entry.getValue(), entry.getKey());
-        }
-        return swapMap;
-    }
-
-    private <K, V> Map<V, Collection<K>> swapKVToVCollectionK(Map<K, V> map) {
-        Map<V, Collection<K>> swapMap = new HashMap<>();
-        for (Map.Entry<K, V> entry : map.entrySet()) {
-            Collection<K> collection = swapMap.get(entry.getValue());
-            if (collection == null) {
-                swapMap.put(entry.getValue(), Collections.singletonList(entry.getKey()));
-            } else {
-                List<K> list = new ArrayList<>(collection);
-                list.add(entry.getKey());
-                swapMap.put(entry.getValue(), list);
-            }
-        }
-        return swapMap;
     }
 
 }
