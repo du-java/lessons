63fb9b273a1e3eb2fa2749bcfbd91f4b52c62db2
du-java <du_java@bk.ru>
2021-04-08 18:11:20 +0200

    lesson17

---
+++ src/lesson17/Car.java
@@ -0,0 +1,8 @@
+package lesson17;
+
+public class Car {
+    private String name;
+    private int doors;
+    private int power;
+    private int wheels;
+}
---
+++ src/lesson17/Person.java
@@ -0,0 +1,12 @@
+package lesson17;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public class Person {
+    private String firstname;
+    private String lastname;
+    private String middleName;
+    private LocalDate birthDay;
+    private List<Person> relations;
+}
