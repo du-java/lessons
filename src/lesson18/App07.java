a444b41d8aa6f75bcff033c443d1f74e0e59c11d
vladakim9 <vladakim705@gmail.com>
2021-04-13 21:00:21 +0300

    Merge remote-tracking branch 'origin/main'

---
+++ src/lesson18/App07.java
@@ -0,0 +1,29 @@
+package lesson18;
+
+import javafx.beans.binding.StringBinding;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class App07 {
+    public static void main(String[] args) {
+        try(
+        FileReader abc= new FileReader("file.txt")){
+            int read = abc.read();
+            StringBuilder stringBuilder = new StringBuilder();
+            while (read != -1){
+                stringBuilder.append((char)read);
+                read= abc.read();
+            }
+            System.out.println(stringBuilder);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+}
