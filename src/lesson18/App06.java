2fdc5e035597d7bd7b4e3cc3856b3ca504055fae
du-java <du_java@bk.ru>
2021-04-13 21:04:12 +0200

    lesson18

--- src/lesson18/App06.java
+++ src/lesson18/App06.java
@@ -2,20 +2,17 @@ package lesson18;
 
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
 import java.util.Scanner;
 
 public class App06 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter text");
         String line = scanner.nextLine();
-       try ( FileOutputStream fos = new FileOutputStream("file.txt")){
-           fos.write(line.getBytes(StandardCharsets.UTF_8));
-       }
-        catch (IOException ex){
-
+        try (FileOutputStream fos = new FileOutputStream("file.txt")) {
+            fos.write(line.getBytes());
+        } catch (IOException ex) {
+            System.err.println(ex.getMessage());
         }
-
-
     }
 }
