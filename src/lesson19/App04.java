92a2f733d522e4d8a1cdde4a824f1d49614cbadd
Dimacom <lqs5283829>
2021-04-15 21:01:50 +0300

    commit new2

--- src/lesson19/App04.java
+++ src/lesson19/App04.java
@@ -2,20 +2,23 @@ package lesson19;
 
 import java.io.*;
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class App04 {
     public static void main(String[] args) throws IOException {
         write(Arrays.asList(
-        new Student("Sasha" ,"Ivanov", LocalDate.of(2000,1,1),"g1",1.58,175),
-        new Student("Masha" ,"Ivanova", LocalDate.of(2001,11,11),"g1",5.8,155)));
+                new Student("Sasha", "Ivanov", LocalDate.of(2000, 1, 1), "g1", 1.58, 175),
+                new Student("Masha", "Ivanova", LocalDate.of(2001, 11, 11), "g1", 5.8, 155)));
+        System.out.println(read());
     }
-     private static void write (List<Student>list) throws IOException {
-        try(
-         DataOutputStream dataOutputStream = new DataOutputStream(new FileOutputStream("data.bin"));)
-        {
-            for(int i=0; i< list.size();i++){
+
+    private static void write(List<Student> list) throws IOException {
+        try (
+                DataOutputStream dataOutputStream = new DataOutputStream(new FileOutputStream("data.bin"));) {
+            dataOutputStream.writeInt(list.size());
+            for (int i = 0; i < list.size(); i++) {
                 dataOutputStream.writeUTF(list.get(i).getFirstname());
                 dataOutputStream.writeUTF(list.get(i).getLastname());
                 dataOutputStream.writeLong(list.get(i).getBirthday().toEpochDay());
@@ -25,5 +28,19 @@ public class App04 {
             }
         }
 
-     }
+    }
+
+    private static List<Student> read() {
+        List<Student> students = new ArrayList<>();
+
+        try (DataInputStream dis = new DataInputStream(new FileInputStream("data.bin"))) {
+            for (int i = 0; i < dis.readInt(); i++) {
+                students.add(new Student(dis.readUTF(), dis.readUTF(), LocalDate.ofEpochDay(dis.readLong()), dis.readUTF(), dis.readDouble(), dis.readLong()));
+            }
+        } catch (IOException ex) {
+            System.err.println(ex.getMessage());
+        }
+        return students;
+    }
 }
+
