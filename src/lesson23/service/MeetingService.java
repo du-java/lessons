19a0476c2ba96fd0be5aada98c0d7c22aeadc8a0
AlexOak <dubovec995@gmail.com>
2021-04-29 21:45:31 +0300

    Lesson23

--- src/lesson23/service/MeetingService.java
+++ src/lesson23/service/MeetingService.java
@@ -1,18 +1,33 @@
 package lesson23.service;
 
 import lesson23.model.Meeting;
+import lesson23.repository.MeetingRepository;
 import lesson23.repository.Repository;
 
 import java.time.LocalDate;
+import java.util.Collections;
 import java.util.List;
 
 public class MeetingService extends AbstractService<Meeting> {
 
-    public MeetingService(Repository<Meeting> repository) {
-        super(repository);
+    private final MeetingRepository meetingRepository;
+
+    public MeetingService(MeetingRepository meetingRepository) {
+        super(meetingRepository);
+        this.meetingRepository = meetingRepository;
     }
 
     public List<Meeting> getAllMeetingsByDate(LocalDate localDate) {
-        return null;
+        if (localDate == null) return Collections.emptyList();
+        return meetingRepository.getAllBy(x -> {
+            LocalDate start = x.getStart().toLocalDate();
+            LocalDate end = x.getEnd().toLocalDate();
+            return start.equals(localDate) ||
+                    end.equals(localDate) ||
+                    (start.isBefore(localDate) &&
+                            end.isAfter(localDate));
+
+        });
+
     }
 }
