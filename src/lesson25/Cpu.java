c4ade8f0f17f5e871cd63c995498934db56a032b
du-java <du_java@bk.ru>
2021-05-11 20:24:03 +0200

    lesson25

--- src/lesson25/Cpu.java
+++ src/lesson25/Cpu.java
@@ -4,16 +4,4 @@ public class Cpu {
     private String modelName;
     private int generation;
     private int frequency;
-
-    public String getModelName() {
-        return modelName;
-    }
-
-    public int getGeneration() {
-        return generation;
-    }
-
-    public int getFrequency() {
-        return frequency;
-    }
 }
