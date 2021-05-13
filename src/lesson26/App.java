7c9eeead95383fab1f65795c9db3922ce14ca296
du-java <du_java@bk.ru>
2021-05-13 21:11:53 +0200

    lesson26

--- src/lesson26/App.java
+++ src/lesson26/App.java
@@ -1,9 +1,15 @@
 package lesson26;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
+import com.fasterxml.jackson.datatype.jsr310.JSR310Module;
+
+import java.io.IOException;
+import java.nio.file.Paths;
 import java.util.List;
 
 public class App {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         final boolean isValid = XmlValidator.validate("resources/results.xml", "resources/results.xsd");
         System.out.println(isValid ? "XML is valid" : "XML is not valid");
 
@@ -11,5 +17,18 @@ public class App {
         final List<Student> students = resultHandler.read("resources/students/results.xml");
 
         System.out.println(students);
+
+        XmlMapper xmlMapper = new XmlMapper();
+        xmlMapper.registerModule(new JSR310Module());
+        xmlMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+
+        final String xml = xmlMapper.writeValueAsString(students);
+
+        System.out.println(xml);
+
+        final Results result = xmlMapper.readValue(Paths.get("resources/students/results.xml").toFile(), Results.class);
+
+        System.out.println(result.getStudents());
+
     }
 }
---
+++ src/lesson26/Results.java
@@ -0,0 +1,15 @@
+package lesson26;
+
+import java.util.List;
+
+public class Results {
+    private List<Student> students;
+
+    public List<Student> getStudents() {
+        return students;
+    }
+
+    public void setStudents(List<Student> students) {
+        this.students = students;
+    }
+}
