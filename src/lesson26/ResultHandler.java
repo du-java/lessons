f3960201fccf66c56f0b017a19f23dba793ce596
du-java <du_java@bk.ru>
2021-05-13 20:34:14 +0200

    lesson26

---
+++ src/lesson26/ResultHandler.java
@@ -0,0 +1,91 @@
+package lesson26;
+
+import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
+
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class ResultHandler extends DefaultHandler {
+
+    private String value;
+    private Student student;
+    private List<Student> students;
+    private List<Test> tests;
+
+    private enum Tags {
+        STUDENT, TESTS, TEST, LOGIN, RESULTS, STUDENTS, GROUP
+    }
+
+    @Override
+    public void startElement(String uri, String localName, String tag, Attributes attributes) throws SAXException {
+        switch (Tags.valueOf(tag.toUpperCase())) {
+            case STUDENTS:
+                students = new ArrayList<>();
+                break;
+            case STUDENT:
+                student = new Student();
+                break;
+            case TESTS:
+                tests = new ArrayList<>();
+                break;
+            case TEST:
+                final Test test = new Test();
+                test.setName(attributes.getValue("name"));
+                test.setDate(LocalDate.parse(attributes.getValue("date")));
+                test.setMark(Double.parseDouble(attributes.getValue("mark")));
+                tests.add(test);
+                break;
+            case LOGIN:
+                student.setFirstname(attributes.getValue("firstname"));
+                student.setLastname(attributes.getValue("lastname"));
+        }
+    }
+
+    @Override
+    public void endElement(String uri, String localName, String tag) throws SAXException {
+        final Tags tags = Tags.valueOf(tag.toUpperCase());
+        switch (tags) {
+            case STUDENT:
+                students.add(student);
+                break;
+            case LOGIN:
+                student.setLogin(value);
+                break;
+            case GROUP:
+                student.setGroup(value);
+                break;
+            case TESTS:
+                student.setTests(tests);
+                break;
+        }
+    }
+
+    @Override
+    public void characters(char[] ch, int start, int length) throws SAXException {
+        value = new String(ch, start, length);
+    }
+
+    public List<Student> read(String xml) {
+        try {
+            final SAXParser parser = SAXParserFactory
+                    .newInstance()
+                    .newSAXParser();
+
+            parser.parse(Paths.get(xml).toFile(), this);
+            return students;
+
+        } catch (ParserConfigurationException | SAXException | IOException ex) {
+            System.err.println(ex.getMessage());
+            return Collections.emptyList();
+        }
+    }
+}
