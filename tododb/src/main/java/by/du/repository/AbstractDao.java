eb6f9b659a1c8218e0407fb1421ad748f05699df
Dimacom <lqs5283829>
2021-06-03 21:49:39 +0300

    new commit

--- tododb/src/main/java/by/du/repository/AbstractDao.java
+++ tododb/src/main/java/by/du/repository/AbstractDao.java
@@ -6,10 +6,9 @@ import by.du.model.Meeting;
 import by.du.util.DbConn;
 import lombok.extern.slf4j.Slf4j;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -28,30 +27,91 @@ public abstract class AbstractDao<T extends Event> implements Dao<T> {
     }
 
     protected abstract String getSqlSelectById();
+
+    protected abstract String getSqlSelectAll();
+
+    protected abstract String getSqlSelectAllBetween();
+
+    protected abstract String getSqlInsert();
+
+    protected abstract String getSqlDelete();
+
+    protected abstract String getSqlUpdate();
+
     protected abstract Optional<T> findById(int id, PreparedStatement st) throws SQLException;
 
     @Override
     public List<T> findAll() {
-        return null;
+        final Connection connection = DbConn.INSTANCE.getConnection();
+        try (final PreparedStatement st = connection.prepareStatement(getSqlSelectAll())) {
+            try (final ResultSet rs = st.executeQuery()) {
+                return findAll(rs);
+            }
+        } catch (SQLException ex) {
+            log.error("Error finding all events", ex);
+            throw new DbException(ex);
+        }
     }
+
+    protected abstract List<T> findAll(ResultSet rs) throws SQLException;
 
     @Override
     public List<T> findAllBetween(LocalDateTime from, LocalDateTime to) {
-        return null;
+        final Connection connection = DbConn.INSTANCE.getConnection();
+        try (final PreparedStatement st = connection.prepareStatement(getSqlSelectAllBetween())) {
+            st.setTimestamp(1, Timestamp.valueOf(from));
+            st.setTimestamp(2, Timestamp.valueOf(to));
+            try (final ResultSet rs = st.executeQuery()) {
+                return findAll(rs);
+            }
+        } catch (SQLException ex) {
+            log.error("Error finding all events between", ex);
+            throw new DbException(ex);
+        }
     }
+
 
     @Override
     public T create(T t) {
-        return null;
+        final Connection connection = DbConn.INSTANCE.getConnection();
+        try (final PreparedStatement st = connection.prepareStatement(getSqlInsert(), Statement.RETURN_GENERATED_KEYS)) {
+            return create(st, t);
+        } catch (SQLException ex) {
+            log.error("Error while creating meeting", ex);
+            throw new DbException(ex);
+        }
     }
+
+    protected abstract T create(PreparedStatement st, T t) throws SQLException;
 
     @Override
     public void delete(T t) {
-
+        final Connection connection = DbConn.INSTANCE.getConnection();
+        try (final PreparedStatement st = connection.prepareStatement(getSqlDelete())) {
+            st.setInt(1, t.getId());
+            final int count = st.executeUpdate();
+            if (count > 1) {
+                log.error("Error while deleting meeting [{}]", t);
+                throw new DbException();
+            }
+        } catch (SQLException ex) {
+            log.error("Error while deleting meeting", ex);
+            throw new DbException(ex);
+        }
     }
 
     @Override
     public T update(T t) {
-        return null;
+        final Connection connection = DbConn.INSTANCE.getConnection();
+        try (final PreparedStatement st = connection.prepareStatement(getSqlUpdate())) {
+            return update(st, t);
+        } catch (SQLException ex) {
+            log.error("Error while updating meeting", ex);
+            throw new DbException(ex);
+        }
     }
+
+    protected abstract T update(PreparedStatement st, T t) throws SQLException;
+
+
 }
--- tododb/src/main/java/by/du/repository/TaskDao2.java
+++ tododb/src/main/java/by/du/repository/TaskDao2.java
@@ -1,12 +1,17 @@
 package by.du.repository;
 
+import by.du.exception.DbException;
 import by.du.model.Task;
+import lombok.extern.slf4j.Slf4j;
 
+import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
-
+@Slf4j
 public class TaskDao2 extends AbstractDao<Task> {
 
     private static final String SELECT_BY_ID = "select * from task where id = ?";
@@ -23,18 +28,85 @@ public class TaskDao2 extends AbstractDao<Task> {
     }
 
     @Override
+    protected String getSqlSelectAll() {
+        return SELECT_ALL ;
+    }
+
+    @Override
+    protected String getSqlSelectAllBetween() {
+        return SELECT_BETWEEN;
+    }
+
+    @Override
+    protected String getSqlInsert() {
+        return INSERT;
+    }
+    @Override
+    protected String getSqlUpdate() {
+        return UPDATE;
+    }
+    @Override
     protected Optional<Task> findById(int id, PreparedStatement st) throws SQLException {
         st.setInt(1, id);
         try (final ResultSet rs = st.executeQuery()) {
             if (rs.next()) {
-                return Optional.of(Task.builder()
-                        .id(rs.getInt("id"))
-                        .isDone(rs.getBoolean("isDone"))
-                        .desc(rs.getString("description"))
-                        .date(rs.getDate("start_date").toLocalDate())
-                        .build());
+                return Optional.of(buildTask(rs));
             }
             return Optional.empty();
         }
+    }
+
+    @Override
+    protected List<Task> findAll(ResultSet rs) throws SQLException {
+        final ArrayList<Task> tasks = new ArrayList<>();
+        while (rs.next()) {
+            tasks.add(buildTask(rs));
+        }
+        return tasks;
+    }
+
+    @Override
+    protected Task create(PreparedStatement st, Task task) throws SQLException {
+        st.setBoolean(1, task.getIsDone());
+        st.setString(2, task.getDesc());
+        st.setDate(3, Date.valueOf(task.getDate()));
+        st.executeUpdate();
+        try (final ResultSet keys = st.getGeneratedKeys()) {
+            if (keys.next()) {
+                final int id = keys.getInt(1);
+                task.setId(id);
+                return task;
+            }
+            log.error("Error while creating task [{}]", task);
+            throw new DbException();
+        }
+    }
+
+    @Override
+    protected Task update(PreparedStatement st, Task task) throws SQLException {
+        st.setBoolean(1, task.getIsDone());
+        st.setString(2, task.getDesc());
+        st.setDate(3, Date.valueOf(task.getDate()));
+        st.setInt(4, task.getId());
+        final int count = st.executeUpdate();
+        if (count > 1) {
+            log.error("Error while updating task [{}]", task);
+            throw new DbException();
+        }
+        return task;
+    }
+
+    private Task buildTask(ResultSet rs) throws SQLException {
+        return Task.builder()
+                .id(rs.getInt("id"))
+                .isDone(rs.getBoolean("isDone"))
+                .desc(rs.getString("description"))
+                .date(rs.getDate("start_date").toLocalDate())
+                .build();
+    }
+
+    @Override
+    protected String getSqlDelete() {
+        return DELETE;
     }
 }
