09ed410a350b100abb7d8e2dee1da4bbebab79e8
Dimacom <lqs5283829>
2021-06-15 21:03:25 +0300

    new commit

--- testing/src/main/java/by/du/Solution.java
+++ testing/src/main/java/by/du/Solution.java
@@ -8,28 +8,17 @@ public class Solution {
      */
 
     public static int solution(int given) {
-        String[] digits = String.valueOf(given).split("");
-
+        String value = String.valueOf(given);
+        String[] digits = value.split("");
+        StringBuilder reversGiven = new StringBuilder(value ).reverse();
+        String[] reversDigits = reversGiven.toString().split("");
         StringBuilder builder = new StringBuilder();
-
-//        int half = digits.length / 2;
-//        if (digits.length % 2 != 0) {
-//            half++;
-//        }
-
         for (int i = 0; i < digits.length; i++) {
-            if (i % 2 != 0) {
-                if (i > (digits.length - i - 1)) break;
-                builder.append(digits[digits.length - i]);
                 builder.append(digits[i]);
-            }
-            if (i % 2 == 0) {
-                if (i > digits.length / 2) break;
-                builder.append(digits[i]);
-            }
-
+            if (builder.length() == digits.length) break;
+            builder.append(reversDigits[i]);
+            if (builder.length() == digits.length) break;
         }
-
         return Integer.parseInt(builder.toString());
     }
 }
